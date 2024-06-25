91d8e3760030b45ed09d55f14be057f05e4f20a7
Raffy Ramos <raffyramos@Raffys-MacBook-Pro.local>
2024-06-26 00:08:44 +0800

    06-26-2024 24:08

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
-//popup
-// const btn = document.querySelector(".popbtn");
-// const listings = document.querySelector(".listings");
+//POPUP
+const openBtn = document.querySelector(".popup-open");
+const closeBtn = document.querySelector(".popup__close-btn");
+const popup = document.querySelector(".popup");
 
-// btn.addEventListener("click", function () {
-//   listings.classList.toggle("hidden");
-// });
+openBtn.addEventListener("click", function () {
+  popup.classList.remove("hidden");
+});
+
+closeBtn.addEventListener("click", function () {
+  popup.classList.add("hidden");
+});
+
+//NAV
+const navBtn = document.querySelector(".navigation__toggle");
+const navigation = document.querySelector(".navigation");
+const navLinks = document.querySelectorAll(".nav__link");
+
+navBtn.addEventListener("click", function () {
+  navigation.classList.toggle("hidden");
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    navigation.classList.add("hidden");
+  });
+}
